5c74a05beb3c5c3153858390a20655fdb2e54aca
Tamil1503 <tamilzarasus@gmail.com>
2019-04-20 23:19:06 +0530

    Files added to complete list screen development

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListEmployeesComponent } from './employees/list-employees.component';
+import { CreateEmployeeComponent } from './employees/create-employee.component';
+import { RouterModule,Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {    path: 'List', component: ListEmployeesComponent  },
+  {    path: 'Create', component: CreateEmployeeComponent  },
+  {    path: '', redirectTo: '/List', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListEmployeesComponent,
+    CreateEmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/employees/list-employees.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../models/employee.model';
+
+@Component({
+  selector: 'app-list-employees',
+  templateUrl: './list-employees.component.html',
+  styleUrls: ['./list-employees.component.css']
+})
+export class ListEmployeesComponent implements OnInit {
+employees:Employee[]=[
+  {
+    id: 1,
+    name: 'Mark',
+    gender: 'Male',
+    email: 'mark@gmail.com',
+    phoneNumber: '2345678',
+    contactPreference: 'Email',
+    dateOfBirth: new Date('01/01/1990'),
+    department: 'IT',
+    isActive: true,
+    photoPath: 'assets/images/Male.jpg'
+},
+{
+  id: 2,
+  name: 'John',
+  gender: 'Male',
+  email: 'john@gmail.com',
+  phoneNumber: '+1985674523',
+  contactPreference: 'Email',
+  dateOfBirth: new Date('01/01/1992'),
+  department: 'IT',
+  isActive: true,
+  photoPath: 'assets/images/images.jpg'
+},
+{
+  id: 3,
+  name: 'Smith',
+  gender: 'Male',
+  email: 'Smith@gmail.com',
+  phoneNumber: '+758145155',
+  contactPreference: 'Email',
+  dateOfBirth: new Date('01/01/1992'),
+  department: 'IT',
+  isActive: true,
+  photoPath: 'assets/images/Female.png'
+}
+];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
